ab69b1e97fa902c3f7c107d570201bad5b85cd52
ariefminardi <ariefminardi140@gmail.com>
2025-07-16 20:27:55 +0700

    feat: menambahkan fitur conver file dan juga format ulang

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import telegram
 from dotenv import load_dotenv
 from telegram.ext import (
 	Application,
-	CommandHandler,
 	CallbackQueryHandler,
+	CommandHandler,
 	ConversationHandler,
 	MessageHandler,
 	filters,
@@ -14,42 +14,83 @@ from telegram.ext import (
 
 load_dotenv()
 
-from handle import cancel, help, start, tiktok, download_tiktok_video, download_tiktok_music
+from handle import (
+	cancel,
+	cv_docx_to_pdf,
+	docx_to_pdf,
+	download_tiktok_music,
+	download_tiktok_video,
+	help,
+	start,
+	tiktok,
+	tiktok_media,
+)
 
+# LOOGGING BOT
 logging.basicConfig(
 	format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-ONE = range(1)
+# STATE CONVERSATION
+TIKTOK = range(1)
+INPUT_FILE = range(1)
 
 
 def main() -> None:
+	# BUILD BOT
 	application = Application.builder().token(os.getenv("TOKEN")).build()
 
-	# REGISTER COMMAND
-	application.add_handler(CommandHandler("start", start))
-	application.add_handler(CommandHandler("help", help))
-
-	# REGISTER CALLBACK BUTTON
-	application.add_handler(CallbackQueryHandler(start, pattern="start"))
-	application.add_handler(CallbackQueryHandler(help, pattern="help"))
-	application.add_handler(CallbackQueryHandler(download_tiktok_music, pattern="^tiktok_music$"))
-
-	# REGISTER CONVERSATION
-	tiktok_conv = ConversationHandler(
+	# REGISTER CONVERSATION HANDLER FIRST (SPECIFIC)
+	tiktok_conv = ConversationHandler(  # TIKTOK
+		allow_reentry=True,
 		entry_points=[
-			CommandHandler("tiktok", tiktok),
 			CallbackQueryHandler(tiktok, pattern="^tiktok$"),
+			CommandHandler("tiktok", tiktok),
 		],
 		states={
-			ONE: [MessageHandler(filters.TEXT & ~filters.COMMAND, download_tiktok_video)],
+			TIKTOK: [
+				MessageHandler(filters.TEXT & ~filters.COMMAND, tiktok_media),
+			],
 		},
 		fallbacks=[
-			CallbackQueryHandler(cancel, pattern="cancel"),
+			CallbackQueryHandler(cancel, pattern="^cancel$"),
 			CommandHandler("cancel", cancel),
 		],
 	)
-	application.add_handler(tiktok_conv)
+
+	docx_to_pdf_conv = ConversationHandler(  # CV DOCX TO PDF
+		allow_reentry=True,
+		entry_points=[
+			CallbackQueryHandler(docx_to_pdf, pattern="^docxtopdf$"),
+			CommandHandler("docxtopdf", docx_to_pdf),
+		],
+		states={INPUT_FILE: [MessageHandler(filters.Document.DOCX, cv_docx_to_pdf)]},
+		fallbacks=[
+			CallbackQueryHandler(cancel, pattern="^cancel$"),
+			CommandHandler("cancel", cancel),
+		],
+	)
+
+	#  REGISTER CONVERSATION
+	application.add_handlers([tiktok_conv, docx_to_pdf_conv])
+
+	# REGISTER SPECIFIC CALLBACK HANDLERS
+	application.add_handlers(
+		[
+			CallbackQueryHandler(download_tiktok_music, pattern="^tiktok_music$"),
+			CallbackQueryHandler(download_tiktok_video, pattern="^tiktok_video$"),
+		]
+	)
+
+	# REGISTER GENERAL COMMANDS
+	application.add_handlers(
+		[
+			CommandHandler("start", start),
+			CommandHandler("help", help),
+			CallbackQueryHandler(start, pattern="^start$"),
+			CallbackQueryHandler(help, pattern="^(help|help_after_download)$"),
+		]
+	)
 
 	application.run_polling(allowed_updates=telegram.Update.ALL_TYPES)
 
