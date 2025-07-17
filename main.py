64c06c239117906c8a2692de280091e63cb2e3a1
ariefminardi <ariefminardi140@gmail.com>
2025-07-17 14:21:08 +0700

    fix io blocking dan juga membuat maks user interaksi bersamaan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import asyncio
 
 import telegram
 from dotenv import load_dotenv
@@ -24,6 +25,7 @@ from handle import (
 	start,
 	tiktok,
 	tiktok_media,
+	status
 )
 
 # LOOGGING BOT
@@ -38,18 +40,28 @@ INPUT_FILE = range(1)
 
 def main() -> None:
 	# BUILD BOT
-	application = Application.builder().token(os.getenv("TOKEN")).build()
+	application = (
+		Application.builder()
+		.token(os.getenv("TOKEN"))
+		.concurrent_updates(25) # maks interaksi bersamaan
+		.http_version("2") # http version
+		.pool_timeout(15) # timeout 15 dtk ketika sistem overload
+		.read_timeout(15) # timeout 15 dtk ketika network overload
+		.update_queue(asyncio.Queue(30)) # set maks antrian
+		.build()
+	)
 
 	# REGISTER CONVERSATION HANDLER FIRST (SPECIFIC)
 	tiktok_conv = ConversationHandler(  # TIKTOK
 		allow_reentry=True,
+		block=False,
 		entry_points=[
 			CallbackQueryHandler(tiktok, pattern="^tiktok$"),
 			CommandHandler("tiktok", tiktok),
 		],
 		states={
 			TIKTOK: [
-				MessageHandler(filters.TEXT & ~filters.COMMAND, tiktok_media),
+				MessageHandler(filters.TEXT & ~filters.COMMAND, tiktok_media, block=False),
 			],
 		},
 		fallbacks=[
@@ -60,11 +72,12 @@ def main() -> None:
 
 	docx_to_pdf_conv = ConversationHandler(  # CV DOCX TO PDF
 		allow_reentry=True,
+		block=False,
 		entry_points=[
 			CallbackQueryHandler(docx_to_pdf, pattern="^docxtopdf$"),
 			CommandHandler("docxtopdf", docx_to_pdf),
 		],
-		states={INPUT_FILE: [MessageHandler(filters.Document.DOCX, cv_docx_to_pdf)]},
+		states={INPUT_FILE: [MessageHandler(filters.Document.DOCX, cv_docx_to_pdf, block=False)]},
 		fallbacks=[
 			CallbackQueryHandler(cancel, pattern="^cancel$"),
 			CommandHandler("cancel", cancel),
@@ -77,18 +90,19 @@ def main() -> None:
 	# REGISTER SPECIFIC CALLBACK HANDLERS
 	application.add_handlers(
 		[
-			CallbackQueryHandler(download_tiktok_music, pattern="^tiktok_music$"),
-			CallbackQueryHandler(download_tiktok_video, pattern="^tiktok_video$"),
+			CallbackQueryHandler(download_tiktok_music, pattern="^tiktok_music$", block=False),
+			CallbackQueryHandler(download_tiktok_video, pattern="^tiktok_video$", block=False),
+			CallbackQueryHandler(status, pattern="^status$", block=False),
 		]
 	)
 
 	# REGISTER GENERAL COMMANDS
 	application.add_handlers(
 		[
-			CommandHandler("start", start),
-			CommandHandler("help", help),
-			CallbackQueryHandler(start, pattern="^start$"),
-			CallbackQueryHandler(help, pattern="^(help|help_after_download)$"),
+			CommandHandler("start", start, block=False),
+			CommandHandler("help", help, block=False),
+			CallbackQueryHandler(start, pattern="^start$", block=False),
+			CallbackQueryHandler(help, pattern="^(help|help_after_download)$", block=False),
 		]
 	)
 
