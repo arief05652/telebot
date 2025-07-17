0042494f7f1d2fb778467d36e89316b8652a06c3
ariefminardi <ariefminardi140@gmail.com>
2025-07-17 19:01:19 +0700

    fitur baru status untuk memantau bot

--- handle/general/status.py
+++ handle/general/status.py
@@ -3,25 +3,26 @@ import psutil, asyncio, threading
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
+
 # STATUS COMMANDS
 async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    app = context.application
+	query = update.callback_query
+	app = context.application
 
-    # Dapatkan antrian user
-    queue_size = app.update_queue.qsize()
-    
-    caption = f"""
+	# Dapatkan antrian user
+	queue_size = app.update_queue.qsize()
+
+	caption = f"""
 💻 <b>System Status Report</b>
 
 📊 <b>Resource Usage</b>
 - CPU: {psutil.cpu_percent()} %
 - RAM: {psutil.virtual_memory().percent} %
-- DISK: {psutil.disk_usage('/').percent} %
+- DISK: {psutil.disk_usage("/").percent} %
 
 🤖 <b>Bot Metrics</b>
 - Active Task: {len(asyncio.all_tasks())}
-- Event Loop: {asyncio.get_event_loop().is_running() and 'Running' or 'Stopped'}
+- Event Loop: {asyncio.get_event_loop().is_running() and "Running" or "Stopped"}
 - Thread Count: {threading.active_count()}
 
 🚦 <b>Queue Status</b>
@@ -32,22 +33,18 @@ async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
 🟢 Status Bot: @NexusUpdatee
 """
 
-    button = [
-        [
-            InlineKeyboardButton("Help menu", callback_data="help"),
+	button = [
+		[
+			InlineKeyboardButton("Help menu", callback_data="help"),
 			InlineKeyboardButton("🧧 Donate", url="https://tako.id/cliari"),
-        ]
-    ]
+		]
+	]
 
-    if query and query.data == "status":
-        await update.callback_query.edit_message_text(
-            text=caption,
-            parse_mode="HTML",
-            reply_markup=InlineKeyboardMarkup(button)
-        )
-    else:
-        await update.message.reply_text(
-            text=caption,
-            parse_mode="HTML",
-            reply_markup=InlineKeyboardMarkup(button)
-        )
+	if query and query.data == "status":
+		await update.callback_query.edit_message_text(
+			text=caption, parse_mode="HTML", reply_markup=InlineKeyboardMarkup(button)
+		)
+	else:
+		await update.message.reply_text(
+			text=caption, parse_mode="HTML", reply_markup=InlineKeyboardMarkup(button)
+		)
